a4dc684d1a44a0ebc58801925e150d817dcad9dd
Rigaut <olivier.rigaut@sony.com>
2019-01-06 18:24:57 -0800

    Added support for google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,5 +121,25 @@ module.exports = {
     },
     `gatsby-plugin-eslint`,
     `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-131732738-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Enables Google Optimize using your container Id
+        optimizeId: '',
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'euskal64500.github.io',
+      },
+    },
   ],
 }
